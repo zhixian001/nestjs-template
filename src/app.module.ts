72a69475c6ab3a1c0e337d1e368294e34a3ee6a0
Darren <darren@platfarm.net>
2022-12-16 11:27:33 +0900

    setup database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import configuration, { DatabaseConfig } from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import DatabaseLogger from './utils/databaseLogger';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          load: [configuration],
+        }),
+      ],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<DatabaseConfig>('database.postgres').host,
+        port: configService.get<DatabaseConfig>('database.postgres').port,
+        username: configService.get<DatabaseConfig>('database.postgres').user,
+        password:
+          configService.get<DatabaseConfig>('database.postgres').password,
+        entities: [],
+        synchronize: true,
+        logging: true,
+        logger: new DatabaseLogger(),
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/configuration.ts
@@ -0,0 +1,24 @@
+export interface DatabaseConfig {
+  host: string;
+  port: number;
+  database: string;
+  user: string;
+  password: string;
+}
+
+export default () => ({
+  port: parseInt(process.env.PORT, 10) || 3000,
+  database: {
+    postgres: {
+      host: process.env.DATABASE_HOST ?? process.env.PGHOST ?? 'localhost',
+      port:
+        parseInt(process.env.DATABASE_PORT, 10) ||
+        parseInt(process.env.PGPORT, 10) ||
+        5432,
+      database: process.env.DATABASE_DATABASE ?? process.env.PGDATABASE,
+      user: process.env.DATABASE_USER || process.env.PGUSER || 'postgres',
+      password:
+        process.env.DATABASE_PASSWORD || process.env.PGPASSWORD || 'postgres',
+    } as DatabaseConfig,
+  } as Record<string, DatabaseConfig>,
+});
