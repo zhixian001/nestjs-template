e38d71d353f3e9dc4256808b67d75c8afe26d2e6
Darren <darren@platfarm.net>
2022-12-16 13:14:17 +0900

    update test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
+    const usersServiceMock = {};
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-    }).compile();
+      providers: [UsersService],
+    })
+      .overrideProvider(UsersService)
+      .useValue(usersServiceMock)
+      .compile();
 
     controller = module.get<UsersController>(UsersController);
   });
