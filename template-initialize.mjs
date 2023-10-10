0496f0ccfb6e76f19e4a04e7d8cc7da90c77c7ec
zhixian001 <okyjh12@naver.com>
2023-10-10 20:32:09 +0900

    update template initialization script

--- template-initialize.mjs
+++ template-initialize.mjs
@@ -5,11 +5,11 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { exec } from 'child_process';
 
-// Change package name
+// Update package.json
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.info(`🫡 Update package name`);
+console.info(`🫡 Update package name, author, email`);
 const packageJsonFilePath = path.join(__dirname, 'package.json');
 
 const packageJsonFileData = JSON.parse(
@@ -18,7 +18,76 @@ const packageJsonFileData = JSON.parse(
   }),
 );
 
+// update name
 packageJsonFileData.name = __dirname.split('/').pop();
+
+// update author
+// Read author info from .gitconfig file
+let authorName = null;
+let authorEmail = null;
+try {
+  const homePath = process.env.HOME;
+
+  if (!homePath) {
+    throw new Error('Home path does not exist');
+  }
+
+  const gitConfigPath = `${homepath}/.gitconfig`;
+
+  if (!fs.existsSync(gitconfigPath)) {
+    throw new Error('Gitconfig does not exist');
+  }
+
+  const gitConfigFileData = await fs.promises.readFile(gitConfigPath, { encoding: 'utf-8' });
+
+  const parsedGitConfig = gitConfigFileData.split('\n')
+    .map(
+      configLine => configLine
+        .split(' ').join('')
+        .split('\t').join('')
+    )
+    .filter(
+      configLine => {
+        if (configLine.startsWith('[')) {
+          return false;
+        }
+
+        if (configLine.startsWith('#')) {
+          return false;
+        }
+
+        if (!configLine.includes('=')) {
+          return false;
+        }
+
+        return true;
+      }
+    );
+
+  parsedGitConfig.forEach(configLine => {
+    const [key, value] = configLine.split('=');
+
+    if (key.toLowerCase() === 'name') {
+      authorName = value;
+    }
+
+    if (key.toLowerCase() === 'email') {
+      authorEmail = value;
+    }
+  });
+
+} catch (err) {
+  // nothing happens
+}
+
+if (authorName && authorEmail) {
+  packageJsonFileData.author = `${authorName}(${authorEmail})`;
+} else if (authorName) {
+  packageJsonFileData.author = `${authorName}`;
+} else if (authorEmail) {
+  packageJsonFileData.author = `${authorEmail}`;
+}
+
 
 await fs.promises.writeFile(
   packageJsonFilePath,
